52395a1743807961233647c4d9e1718d94d041a3
Alexander Chupikov <spider.ch@gmail.com>
2013-03-15 21:07:54 +0700

    Added realization.

--- lab1/gronsfeld.cpp
+++ lab1/gronsfeld.cpp
@@ -41,11 +41,59 @@ CipherType gronsfeld::getCipher() const
 
 bool gronsfeld::encode()
 {
+	m_encodedMessage.clear();
+
+	if(m_decodedMessage.empty())
+		return false;
+
+	const size_t size = m_decodedMessage.size();
+	const size_t cipherSize = m_cipher.size();
+
+	const char* symbol = m_decodedMessage.c_str();
+
+	for(size_t index = 0, cipherIndex = 0; size != index; ++symbol, ++index, ++cipherIndex){
+		if(cipherSize == cipherIndex){
+			cipherIndex = 0;
+		}
+		/*
+		 * In Unicode cyrillic symbols coded by 2 bytes.
+		 * Skip firt and encode second one. If we encode
+		 * first byte we get garbage in string
+		 */
+		if(*symbol < 0){
+			m_encodedMessage.push_back(*symbol);
+		}
+		m_encodedMessage.push_back(*symbol + m_cipher[cipherIndex]);
+	}
 	return true;
 }
 
 bool gronsfeld::decode()
 {
+	m_decodedMessage.clear();
+
+	if(m_encodedMessage.empty())
+		return false;
+
+	const size_t size = m_encodedMessage.size();
+	const size_t cipherSize = m_cipher.size();
+
+	const char* symbol = m_encodedMessage.c_str();
+
+	for(size_t index = 0, cipherIndex = 0; size != index; ++symbol, ++index, ++cipherIndex){
+		if(cipherSize == cipherIndex){
+			cipherIndex = 0;
+		}
+		/*
+		 * In Unicode cyrillic symbols coded by 2 bytes.
+		 * Skip firt and encode second one. If we encode
+		 * first byte we get garbage in string
+		 */
+		if(*symbol < 0){
+			m_decodedMessage.push_back(*symbol);
+		}
+		m_decodedMessage.push_back(*symbol - m_cipher[cipherIndex]);
+	}
 	return true;
 }
 
